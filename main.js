90746857d92d23681160d433074c30cf7388e788
ainhoavegasanchez <ainhoavega99@gmail.com>
2023-11-30 09:55:42 +0100

    actualizacion por defecto

--- main.js
+++ main.js
@@ -12,17 +12,21 @@ img.src = "./img/user_nt_found.jpg";
 card.append(img);
 
 let array = ["Name", "Mail", "Phone", "Location", "Current Time"];
+let valores= ["name surname", "mail", "phone", "city", "time"];
 
 let lista = document.createElement("ul");
+let i = 0;
 array.forEach((elem) => {
   let listado = document.createElement("li");
   let span = document.createElement("span");
+  span.textContent = valores[i];
   let negrita = document.createElement("strong");
   negrita.textContent = `${elem}: `;
   elem == "Current Time" ? (elem = "current-time") : elem;
   listado.setAttribute("id", elem.toLocaleLowerCase());
   listado.append(negrita,span);
   lista.append(listado);
+  i++;
 });
 card.append(lista);
 
